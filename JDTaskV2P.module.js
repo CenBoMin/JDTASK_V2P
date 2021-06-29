8d710a8b0c9128991770916e1bc0bc67b21914c8
Cenbomin <2564885669@qq.com>
2021-06-30 01:29:30 +0800

    sync

--- JDTaskV2P.module.js
+++ JDTaskV2P.module.js
@@ -40,7 +40,7 @@ function moduleTask() {
             }
           })
         }else if (i == 6) {
-          $message.success("❗️请观察任务运行日志:JDTASKV2P模块安装完成\n👉 点击消息可打开程序运行日志",{ secd: 0, url: `${__home}/logs/${__name}.log` })
+          $message.success("❗️请观察任务运行日志:JDTASKV2P模块安装完成\n👉 点击消息可打开程序运行日志(请刷新)",{ secd: 0, url: `${__home}/logs/${__name}.log` })
           $message.loading("如果模块安装完成！\n请到setting=>初始化相关设置=>重启elecV2P", 5)
         }
       },(i + 1) * 4000);
