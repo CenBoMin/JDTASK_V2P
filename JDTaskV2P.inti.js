b00c5c2c968c86cd7f2b3b80ffa99f4938f44180
Cenbomin <2564885669@qq.com>
2021-06-29 21:25:30 +0800

    初始化文件intish

--- JDTaskV2P.inti.js
+++ JDTaskV2P.inti.js
@@ -3,20 +3,42 @@
 //============================
 $message.loading("JDTASKV2P初始化开始", 2)
 //============================
-
-$exec('chmod +x ./inti.sh', {
-  cwd: 'script/JSFile/JDTASK_V2P',
-  cb(data, error) {
-    error ? console.error(error) : console.log(data)
+//下载inti.sh到script/JSFile
+intiTask();
+function intiTask() {
+  for (let i = 0; i < 4; i++) {
+    (function (i) {
+      setTimeout(function () {
+        if (i == 0) {
+          $message.loading("⏳ 下载初始化文件:inti.sh", 2)
+          $download('https://raw.githubusercontent.com/CenBoMin/JDTASK_V2P/main/inti.sh', {
+            folder: './script/JSFile',
+            name: 'inti.sh'
+          }).then(d=> $message.success("✅ inti.sh已下载script/JSFile", 5)).catch(e=>console.error(e))
+        }else if (i == 1) {
+          $message.loading("⏳ 初始化任务准备安装中...", 5)
+          $exec('chmod +x ./inti.sh', {
+            cwd: 'script/JSFile',
+            cb(data, error) {
+              error ? console.error(error) : console.log(data)
+            }
+          })
+        }else if (i == 2) {
+          $message.loading("⏳ 开始安装...请稍等片刻", 5)
+          $exec('./inti.sh', {
+            cwd: 'script/JSFile',timeout: 0,
+            cb(data, error) {
+              error ? console.error(error) : console.log(data)
+            }
+          })
+        }else if (i == 3) {
+          $message.success("恭喜！JDTASKV2P初始化完成 🎉", 3)
+        }
+      },(i + 1) * 4000);
+    })(i);
   }
-})
+}
 
-$exec('./inti.sh', {
-  cwd: 'script/JSFile/JDTASK_V2P',
-  cb(data, error) {
-    error ? console.error(error) : console.log(data)
-  }
-})
 
 //============================
 //下载需要的环境变量文件：sendNotify.js && jdCookie.js && JS_USER_AGENTS.js &&
--- inti.sh
+++ inti.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+apk update
 echo "🤖 开始安装[git]";
 apk add git
 echo "🤖 [git]安装完成";
-echo "🤖 [gitclone]:JDHelloWorld/jd_scripts 拉库中...";
+
+echo "🤖 [gitclone]:JDHelloWorld/jd_scripts";
 git clone https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts
-echo "🤖 [gitclone]:CenBoMin/JDTASK_V2P 拉库中...";
+
+echo "🤖 [gitclone]:CenBoMin/JDTASK_V2P";
 git clone https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P
