945bbb2de0a0dd04b162bc16fbdd82719bbd4465
Cenbomin <2564885669@qq.com>
2021-06-29 23:25:19 +0800

    test sync

--- task7.js
+++ task7.js
@@ -1,15 +1,4 @@
 // @grant nodejs
-$exec('node jd_big_winner.js', {
-  cwd: 'script/JSFile/jd_scripts', timeout: 0,
-  env: {
-    ...process.env,
-    V2P_NOTIFY: `${__home}/logs/${__name}.log`,
-    JD_COOKIE: $store.get('CookiesJDV2P', 'string')
-  },
-  cb(data, error){
-    error ? console.error(error) : console.log(data)
-  }
-})
 
 $exec('node jd_syj.js', {
   cwd: 'script/JSFile/jd_scripts', timeout: 0,
