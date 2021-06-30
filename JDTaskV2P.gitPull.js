bd972a8dc9d8b5a79e28fedec02fe0dd00b4ea9a
Cenbomin <2564885669@qq.com>
2021-07-01 05:04:17 +0800

    gitpull.Log 3day -> 2day

--- JDTaskV2P.gitPull.js
+++ JDTaskV2P.gitPull.js
@@ -5,7 +5,7 @@
 */
 
 //CenBoMin/JDTASK_V2P
-$exec('git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P && git pull origin main && git log --pretty=format:"%h - %an, %ar : %s" --since=3.day', {
+$exec('git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P && git pull origin main && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day', {
   cwd: 'script/JSFile/JDTASK_V2P', timeout: 0,
   cb(data, error){
     error ? console.error(error) : console.log(data)
@@ -13,7 +13,7 @@ $exec('git config pull.rebase false && git fetch https://ghproxy.com/https://git
 })
 
 //JDHelloWorld/jd_scripts
-$exec('git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts && git pull origin main && git log --pretty=format:"%h - %an, %ar : %s" --since=3.day', {
+$exec('git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts && git pull origin main && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day', {
   cwd: 'script/JSFile/jd_scripts', timeout: 0,
   cb(data, error){
     error ? console.error(error) : console.log(data)
