a1d64e8a2a0f674c8c8d631dc9158e92a16d3188
Cenbomin <2564885669@qq.com>
2021-07-12 23:43:12 +0800

    移除燃动夏季_SH助力smiek2221

--- task7.js
+++ task7.js
@@ -14,21 +14,21 @@ $exec('node jd_summer_movement.js', {
   }
 })
 
-// @grant nodejs
-//燃动夏季_SH助力smiek2221
-$exec('node jd_summer_movement_help.js', {
-  cwd: 'script/JSFile/JDTASK_V2P/JDScriptsBak', timeout: 0,
-  env: {
-    ...process.env,
-    V2P_NOTIFY: `${__home}/logs/${__name.replace(/\//,"-")}.log`,
-    JD_COOKIE: $store.get('CookiesJDV2P', 'string'),
-    summer_movement_ShHelpFlag:1,
-
-  },
-  cb(data, error){
-    error ? console.error(error) : console.log(data)
-  }
-})
+// // @grant nodejs
+// //燃动夏季_SH助力smiek2221
+// $exec('node jd_summer_movement_help.js', {
+//   cwd: 'script/JSFile/JDTASK_V2P/JDScriptsBak', timeout: 0,
+//   env: {
+//     ...process.env,
+//     V2P_NOTIFY: `${__home}/logs/${__name.replace(/\//,"-")}.log`,
+//     JD_COOKIE: $store.get('CookiesJDV2P', 'string'),
+//     summer_movement_ShHelpFlag:1,
+//
+//   },
+//   cb(data, error){
+//     error ? console.error(error) : console.log(data)
+//   }
+// })
 
 
 // #十元街
