954f53bd3dff1e5ca10c11bfd3a7e63a292f4133
Cenbomin <2564885669@qq.com>
2021-07-12 23:05:43 +0800

    sync

--- task6.js
+++ task6.js
@@ -1,5 +1,18 @@
 // @grant nodejs
 
+//惊喜开团
+$exec('node star_dreamFactory_tuan.js', {
+  cwd: 'script/JSFile/JDTASK_V2P/JDScriptsBak', timeout: 0,
+  env: {
+    ...process.env,
+    V2P_NOTIFY: `${__home}/logs/${__name.replace(/\//,"-")}.log`,
+    JD_COOKIE: $store.get('CookiesJDV2P', 'string')
+  },
+  cb(data, error){
+    error ? console.error(error) : console.log(data)
+  }
+})
+
 // #宠汪汪偷好友积分与狗粮smiek2221
 $exec('node jd_joy_steal.js', {
   cwd: 'script/JSFile/JDTASK_V2P/JDScriptsBak', timeout: 0,
