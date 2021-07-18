855c117616431d6520e0fa71f0d9fd39890b7806
Cenbomin <2564885669@qq.com>
2021-07-19 03:46:33 +0800

    sync

--- inti.sh
+++ inti.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 apk update
-echo "🤖 开始安装[git]";
+echo "🌟 开始安装[git]";
 apk add git
 echo "✅ [git]安装完成";
 
 rm -rf JDTASK_V2P && rm -rf jd_scripts && rm -rf faker2
 
-echo "🤖 [gitclone]:smiek2221/scripts";
+echo "🌟 [gitclone]:Aaron-lv/sync";
+git clone -b jd_scripts https://ghproxy.com/https://github.com/Aaron-lv/sync
+
+echo "🌟 [gitclone]:smiek2221/scripts";
 git clone https://ghproxy.com/https://github.com/smiek2221/scripts
 
-echo "🤖 [gitclone]:shufflewzc/faker2";
+echo "🌟 [gitclone]:shufflewzc/faker2";
 git clone https://ghproxy.com/https://github.com/shufflewzc/faker2
 
-echo "🤖 [gitclone]:JDHelloWorld/jd_scripts";
+echo "🌟 [gitclone]:JDHelloWorld/jd_scripts";
 git clone https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts
 
-echo "🤖 [gitclone]:CenBoMin/JDTASK_V2P";
+echo "🌟 [gitclone]:CenBoMin/JDTASK_V2P";
 git clone https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P
 
-echo "🤖 [gitpull]:开始下载库文件";
+echo "🌟 [gitpull]:开始下载库文件";
 
-git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/smiek2221/scripts && git pull origin main
+git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/smiek2221/scripts && git pull origin master
 
 git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts && git pull origin main
 
@@ -28,4 +31,6 @@ git config pull.rebase false && git fetch https://ghproxy.com/https://github.com
 
 git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/shufflewzc/faker2 && git pull origin main
 
-echo "✅ 恭喜！JDTASKV2P初始化完成 🎉 ";
+git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/Aaron-lv/sync && git pull origin jd_scripts
+
+echo "✅ 恭喜！JDTASKV2P拉库完成 🎉 ";
