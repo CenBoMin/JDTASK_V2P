00198d9aaa91a21ef92a0f10807c6a45f0361a22
Cenbomin <2564885669@qq.com>
2021-07-19 04:31:00 +0800

    sync

--- inti.sh
+++ inti.sh
@@ -4,7 +4,8 @@ echo "🌟 开始安装[git]";
 apk add git
 echo "✅ [git]安装完成";
 
-rm -rf JDTASK_V2P && rm -rf jd_scripts && rm -rf faker2
+echo "🌟 [gitpull]:开始下载库文件";
+rm -rf JDTASK_V2P && rm -rf jd_scripts && rm -rf faker2 && rm -rf sync
 
 echo "🌟 [gitclone]:Aaron-lv/sync";
 git clone -b jd_scripts https://ghproxy.com/https://github.com/Aaron-lv/sync
@@ -20,17 +21,5 @@ git clone https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts
 
 echo "🌟 [gitclone]:CenBoMin/JDTASK_V2P";
 git clone https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P
-
-# echo "🌟 [gitpull]:开始下载库文件";
-
-# git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/smiek2221/scripts && git pull origin master:smiek2221 && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
-#
-# git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts && git pull origin main:JDHelloWorld && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
-#
-# git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P && git pull origin main:CenBoMin && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
-#
-# git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/shufflewzc/faker2 && git pull origin main:faker2 && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
-#
-# git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/Aaron-lv/sync && git pull origin jd_scripts:Aaron-lv && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
 
 echo "✅ 恭喜！JDTASKV2P脚本初始化完成 🎉 ";
