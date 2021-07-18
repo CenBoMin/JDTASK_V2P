5aba76647f831130b0132069c2ef22fe3370320d
Cenbomin <2564885669@qq.com>
2021-07-19 03:54:44 +0800

    sync

--- gitpull.sh
+++ gitpull.sh
@@ -1,27 +1,6 @@
 #!/bin/sh
-apk update
-echo "🌟 开始安装[git]";
-apk add git
-echo "✅ [git]安装完成";
 
-rm -rf JDTASK_V2P && rm -rf jd_scripts && rm -rf faker2
-
-echo "🌟 [gitclone]:Aaron-lv/sync";
-git clone -b jd_scripts https://ghproxy.com/https://github.com/Aaron-lv/sync
-
-echo "🌟 [gitclone]:smiek2221/scripts";
-git clone https://ghproxy.com/https://github.com/smiek2221/scripts
-
-echo "🌟 [gitclone]:shufflewzc/faker2";
-git clone https://ghproxy.com/https://github.com/shufflewzc/faker2
-
-echo "🌟 [gitclone]:JDHelloWorld/jd_scripts";
-git clone https://ghproxy.com/https://github.com/JDHelloWorld/jd_scripts
-
-echo "🌟 [gitclone]:CenBoMin/JDTASK_V2P";
-git clone https://ghproxy.com/https://github.com/CenBoMin/JDTASK_V2P
-
-echo "🌟 [gitpull]:开始下载库文件";
+echo "🌟 [gitpull]:开始拉库更新文件";
 
 git config pull.rebase false && git fetch https://ghproxy.com/https://github.com/smiek2221/scripts && git pull origin master && git log --pretty=format:"%h - %an, %ar : %s" --since=2.day
 
