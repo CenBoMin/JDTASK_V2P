78b94d23496eb45cbecb75e29533622b5e3bd0a9
Cenbomin <2564885669@qq.com>
2021-07-20 01:47:29 +0800

    支持使用python的京东脚本

--- JDTaskV2P.test.js
+++ JDTaskV2P.test.js
@@ -100,10 +100,11 @@ async function intiCheck() {
     // 开始安装 python
     $exec('apk add python3 py3-pip', {
       call: true, timeout: 0,
+      cwd: '/script/Shell',
       cb(data, error, finish){
         if (!error && finish) {
           // 安装一些 python 库，根据需要自行选择更改
-          // $exec('pip3 install you-get youtube-dl requests', { cb(data, error){error ? console.error(error) : console.log(data)} })
+          $exec('pip3 install you-get youtube-dl requests', { cb(data, error){error ? console.error(error) : console.log(data)} })
 
           // python 和库安装完成后可直接在系统或其他脚本中调用，不需要再次安装
           // 下面这段代码可在新的脚本中单独运行
