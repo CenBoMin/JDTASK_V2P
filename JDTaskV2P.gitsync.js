ef3951ae9d372c5c7318543813764dba285e168b
Cenbomin <2564885669@qq.com>
2021-07-20 02:18:16 +0800

    sync

--- JDTaskV2P.gitsync.js
+++ JDTaskV2P.gitsync.js
@@ -56,7 +56,28 @@ function intiTask() {
             }
           })
         } else if (i == 7) {
-          // $message.success("git clone完成 ",3)
+          // 开始安装 python
+          $exec('apk add python3 py3-pip', {
+            call: true, timeout: 0,
+            cwd: '/script/Shell',
+            cb(data, error, finish){
+              if (!error && finish) {
+                // 安装一些 python 库，根据需要自行选择更改
+                $exec('pip3 install you-get youtube-dl requests', { cb(data, error){error ? console.error(error) : console.log(data)} })
+
+                // python 和库安装完成后可直接在系统或其他脚本中调用，不需要再次安装
+                // 下面这段代码可在新的脚本中单独运行
+                $exec('python3 -u test.py', {
+                  cwd: './script/Shell',    // test.py 所在目录（其他文件可通过 EFSS 文件管理界面进行上传
+                  cb(data, error){
+                    error ? console.error(error) : console.log(data)
+                  }
+                })
+              } else {
+                error ? console.error(error) : console.log(data)
+              }
+            }
+          })
         }
       }, (i + 1) * 4000);
     })(i);
