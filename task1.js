5ca9340aac4ac9474676f8d2eac661074a178fde
Cenbomin <2564885669@qq.com>
2021-07-20 23:21:15 +0800

    faker2#东东乐园大风车jd_ddnc_farmpark.js 京喜财富岛挂机/Aaron-lv →task3 每小时执行一次

--- task1.js
+++ task1.js
@@ -1,4 +1,18 @@
 // @grant nodejs
+//东东乐园大风车jd_ddnc_farmpark.js
+$exec('node jd_ddnc_farmpark.js', {
+  cwd: 'script/JSFile/faker2', timeout: 0,
+  env: {
+    ...process.env,
+    JD_COOKIE: $store.get('CookiesJDV2P', 'string'),
+    lsjdh: "jdAward3"
+  },
+  cb(data, error){
+    error ? console.error(error) : console.log(data)
+  }
+})
+
+// @grant nodejs
 //店铺签到jd_shop_sign.js
 $exec('node jd_shop_sign.js', {
   cwd: 'script/JSFile/JDTASK_V2P/JDScriptsBak', timeout: 0,
--- task11.js
+++ task11.js
@@ -14,21 +14,6 @@ $exec('node jd_cfd.js', {
 })
 
 // @grant nodejs
-// 京喜财富岛挂机/Aaron-lv
-$exec('node jd_cfd_loop.js', {
-  cwd: 'script/JSFile/sync',
-  timeout: 0,
-  env: {
-    ...process.env,
-    V2P_NOTIFY: `${__home}/logs/${__name.replace(/\//,"-")}.log`,
-    JD_COOKIE: $store.get('CookiesJDV2P', 'string')
-  },
-  cb(data, error) {
-    error ? console.error(error) : console.log(data)
-  }
-})
-
-// @grant nodejs
 // 省钱大赢家之翻翻乐
 $exec('node jd_big_winner.js', {
   cwd: 'script/JSFile/sync',
