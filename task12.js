ab4f45738ef5172cf3caf4af09522cf19a907cc1
Cenbomin <2564885669@qq.com>
2021-07-21 12:10:56 +0800

    改10豆入会

--- task12.js
+++ task12.js
@@ -10,12 +10,12 @@ function opencardTask() {
             name: 'jd_OpenCard.py'
           }).then(d=> console.log("✅ Py文件已下载成功")).catch(e=>console.error(e))
         }else if (i == 2) {
-          console.log("⏳ 开始执行【JD入会领豆】,3豆即入会")
+          console.log("⏳ 开始执行【JD入会领豆】,10豆即入会")
           $exec('python3 -u jd_OpenCard.py', {
             cwd: 'script/Shell', timeout: 0,
             env: {
               JD_COOKIE: $store.get('CookiesJDV2P', 'string'),
-              openCardBean: 3,
+              openCardBean: 10,
               memory: false
             },
             cb(data, error){
